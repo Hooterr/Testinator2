17f4fa4edffaad43e03af277e3f1e8ca02e37c62
Minorsonek <minorsonek@gmail.com>
2019-08-22 15:05:36 +0200

    Added simple dashboard for now

--- Source/Testinator.Web/Controllers/DashboardController.cs
+++ Source/Testinator.Web/Controllers/DashboardController.cs
@@ -1,16 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Testinator.Web
 {
+    /// <summary>
+    /// The controller for main user dashboard page that displays his data
+    /// </summary>
+    [Authorize]
     public class DashboardController : Controller
     {
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public DashboardController()
         {
         }
 
+        #endregion
+
         public IActionResult Index()
         {
-            return View();
+            var model = new DashboardViewModel
+            {
+                FirstName = User.Identity.Name
+            };
+            return View(model);
         }
     }
 }
