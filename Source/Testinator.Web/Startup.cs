09ffd9a44d82c0149fa09e1be4006b37d009577f
Minorsonek <minorsonek@gmail.com>
2019-08-22 11:24:57 +0200

    Dependency injection setup for website project

--- Source/Testinator.Web/Startup.cs
+++ Source/Testinator.Web/Startup.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 using Testinator.Web.Core;
+using Testinator.Web.Database;
 
 namespace Testinator.Web
 {
@@ -20,8 +24,7 @@ namespace Testinator.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IAccountService, AccountService>();
-
+            // Configure cookies policy
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
@@ -29,13 +32,52 @@ namespace Testinator.Web
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            // Add services to DI
+            services.AddScoped<IAccountService, AccountService>();
+
+            // Add TestinatorWebDbContext to DI
+            services.AddDbContext<TestinatorWebDbContext>(options =>
+                // TODO: Add connection string to app settings
+                options.UseSqlServer("Server=.;Database=TestinatorWebDB;Trusted_Connection=True;MultipleActiveResultSets=True;"));
+
+            // AddIdentity adds cookie based authentication
+            // Adds scoped classes for things like UserManager, SignInManager, PasswordHashers etc..
+            // NOTE: Automatically adds the validated user from a cookie to the HttpContext.User
+            // https://github.com/aspnet/Identity/blob/85f8a49aef68bf9763cd9854ce1dd4a26a7c5d3c/src/Identity/IdentityServiceCollectionExtensions.cs
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+
+                // Adds UserStore and RoleStore from this context
+                // That are consumed by the UserManager and RoleManager
+                // https://github.com/aspnet/Identity/blob/dev/src/EF/IdentityEntityFrameworkBuilderExtensions.cs
+                .AddEntityFrameworkStores<TestinatorWebDbContext>()
+
+                // Adds a provider that generates unique keys and hashes for things like
+                // forgot password links, phone number verification codes etc...
+                .AddDefaultTokenProviders();
+
+            // Change password policy
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Make really weak passwords possible
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 5;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+
+                // Make sure users have unique emails
+                options.User.RequireUniqueEmail = true;
+            });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
         {
+            // Setup Identity
+            app.UseAuthentication();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -47,9 +89,15 @@ namespace Testinator.Web
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            // Force non-essential cookies to only store
+            // if the user has consented
             app.UseCookiePolicy();
+
+            // Redirect all calls from HTTP to HTTPS
+            app.UseHttpsRedirection();
+
+            // Serve static files
+            app.UseStaticFiles();
 
             app.UseMvc(routes =>
             {
@@ -57,6 +105,9 @@ namespace Testinator.Web
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            // Make sure we have the database
+            serviceProvider.GetService<TestinatorWebDbContext>().Database.EnsureCreated();
         }
     }
 }
