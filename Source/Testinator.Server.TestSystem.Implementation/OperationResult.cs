85c94fe0778fc82d2375505f494c26c163b82eff
Maksymilian Lach <maks.lach99@gmail.com>
2019-08-25 10:56:36 +0200

    Operation result improvements

--- Source/Testinator.Server.TestSystem.Implementation/OperationResult.cs
+++ Source/Testinator.Server.TestSystem.Implementation/OperationResult.cs
@@ -7,16 +7,31 @@ namespace Testinator.Server.TestSystem.Implementation
     public class OperationResult
     {
         public List<string> Errors { get; private set; } = new List<string>();
-        public bool Success => Errors.Count == 0;
+        public bool Succeeded => Errors.Count == 0;
+        public bool Failed => !Succeeded;
 
-        internal OperationResult()
+        public OperationResult()
+        { }
+
+        public OperationResult(string error)
         {
-
+            Errors.Add(error);
+        }
+        public OperationResult(string[] error)
+        {
+            Errors.AddRange(error);
         }
 
-        internal void AddError(string msg)
+        public void AddError(string msg)
         {
             Errors.Add(msg);
         }
+
+        public static OperationResult Fail(string error) => new OperationResult(error);
+
+        public static OperationResult Fail(string[] errors) => new OperationResult(errors);
+
+        public static OperationResult Success() => new OperationResult();
+
     }
 }
