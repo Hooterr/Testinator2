1148ce4c5220457cd6339fc5fb152dfb5e0ba5d7
Maksymilian Lach <maks.lach99@gmail.com>
2019-08-26 12:42:47 +0200

    Remove unused region

--- Source/Testinator.Server.TestSystem.Implementation/Questions/Task/ImageContent.cs
+++ Source/Testinator.Server.TestSystem.Implementation/Questions/Task/ImageContent.cs
@@ -10,15 +10,12 @@ namespace Testinator.Server.TestSystem.Implementation.Questions.Task
     /// </summary>
     public class ImageContent : IImageContent
     {
-        #region Private Members
 
         [MaxCollectionCount(maxCount: 5)]
         [MaxCollectionCount(maxCount: 1, fromVersion: 2)]
         [MaxCollectionCount(maxCount: 1, fromVersion: 4)]
         [MaxImageSize(width: 1000, height: 1000, fromVersion: 1)]
         public IList<Image> Images { get; internal set; }
-
-        #endregion
 
     }
 }
