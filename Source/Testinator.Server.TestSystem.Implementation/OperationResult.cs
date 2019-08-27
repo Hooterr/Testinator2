1bd359a4881c48aaca5347d178c3c8991040c99d
Maksymilian Lach <maks.lach99@gmail.com>
2019-08-27 22:17:07 +0200

    Operation result modification.

--- Source/Testinator.Server.TestSystem.Implementation/OperationResult.cs
+++ Source/Testinator.Server.TestSystem.Implementation/OperationResult.cs
@@ -41,23 +41,47 @@ namespace Testinator.Server.TestSystem.Implementation
 
         public static OperationResult Fail() => new OperationResult(false);
 
-        public static OperationResult Succes() => new OperationResult(true);
+        public static OperationResult Success() => new OperationResult(true);
 
     }
 
-    public class OperationResult<TOut> : OperationResult
+    public class OperationResult<TOut>
     {
         public readonly TOut Result;
 
-        public OperationResult(string error) : base(error) { }
+        public List<string> Errors { get; private set; } = new List<string>();
+        public bool Succeeded { get; private set; }
+        public bool Failed => !Succeeded;
+
+        public OperationResult(string error)
+        {
+            Errors.Add(error);
+            Succeeded = false;
+        }
 
 
-        public OperationResult(string[] errors) : base(errors) { }
+        public OperationResult(string[] errors)
+        {
+            Errors.AddRange(errors);
+            Succeeded = false;
+        }
 
-        public OperationResult(TOut obj, bool success) : base(success)
+        public OperationResult(TOut obj, bool success)
         {
             Result = obj;;
+            Succeeded = success;
         }
+
+        public OperationResult(bool success)
+        {
+            Succeeded = success;
+        }
+
+        public static OperationResult<TOut> Fail(string error) => new OperationResult<TOut>(error);
+
+        public static OperationResult<TOut> Fail(string[] errors) => new OperationResult<TOut>(errors);
+
+        public static OperationResult<TOut> Fail() => new OperationResult<TOut>(false);
 
         public static OperationResult<TOut> Success(TOut obj) => new OperationResult<TOut>(obj, true);
 
