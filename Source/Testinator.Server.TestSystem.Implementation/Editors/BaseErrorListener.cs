1ae9f6a454babd91d73082067cd5d58c1497c3cc
Maksymilian Lach <maks.lach99@gmail.com>
2019-08-29 21:08:04 +0200

    ErrorListener implementation extracted to a new class

---
+++ Source/Testinator.Server.TestSystem.Implementation/Editors/BaseErrorListener.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using Testinator.Server.TestSystem.Implementation.Attributes;
+
+namespace Testinator.Server.TestSystem.Implementation
+{
+    internal abstract class BaseErrorListener<TIntereface> : IErrorListener<TIntereface>
+    {
+        private readonly Dictionary<string, Action<string>> mErrorHandlers;
+        private readonly Dictionary<string, string> mUnHandledErrorMessages;
+
+        public void OnErrorFor(Expression<Func<TIntereface, object>> propertyExpression, Action<string> action)
+        {
+            var expression = (MemberExpression)propertyExpression.Body;
+            var propertyInfo = (PropertyInfo)expression.Member;
+
+            if (propertyInfo.GetCustomAttributes<EditorFieldAttribute>().Any() == false)
+                throw new ArgumentException($"This property is not an editor property.");
+
+            mErrorHandlers[propertyInfo.Name] = action;
+        }
+
+        protected BaseErrorListener()
+        {
+            mErrorHandlers = new Dictionary<string, Action<string>>();
+            mUnHandledErrorMessages = new Dictionary<string, string>();
+
+            // Get all editor methods and create the error handlers map
+            var methodNames = typeof(TIntereface).GetProperties()
+                              .Where(field => field.GetCustomAttributes<EditorFieldAttribute>().Any())
+                              .Select(field => field.Name)
+                              .ToList();
+
+            mErrorHandlers = methodNames.ToDictionary(k => k, v => default(Action<string>));
+            mUnHandledErrorMessages = methodNames.ToDictionary(k => k, v => default(string));
+        }
+
+        protected void HandleErrorFor(Expression<Func<TIntereface, object>> propertyExpression, string message)
+        {
+            var propertyName = ExpressionHelpers.GetCorrectPropertyName(propertyExpression);
+            // If there is handler for that method
+            if (mErrorHandlers[propertyName] != null)
+                mErrorHandlers[propertyName].Invoke(message);
+            else
+                mUnHandledErrorMessages[propertyName] = message;
+        }
+
+        protected List<string> GetUnhandledErrors()
+        {
+            var UnhanledErrorMessages = mUnHandledErrorMessages.Values.Where(x => !string.IsNullOrEmpty(x)).ToList();
+            return UnhanledErrorMessages;
+        }
+
+        protected void ClearAllErrors()
+        {
+            mUnHandledErrorMessages.Keys.ToList().ForEach(k => mUnHandledErrorMessages[k] = default);
+        }
+    }
+}
--- Source/Testinator.Server.TestSystem.Implementation/Editors/Task/Text/TextEditor.cs
+++ Source/Testinator.Server.TestSystem.Implementation/Editors/Task/Text/TextEditor.cs
@@ -1,23 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Text;
 using Testinator.Server.TestSystem.Implementation.Attributes;
 using Testinator.Server.TestSystem.Implementation.Questions.Task;
 using Testinator.TestSystem.Abstractions.Questions.Task;
 
 namespace Testinator.Server.TestSystem.Implementation
 {
-    internal class TextEditor : ITextEditor, IEditor<ITextContent>
+    internal class TextEditor : BaseErrorListener<ITextEditor>, ITextEditor, IEditor<ITextContent>
     {
         private readonly int mVersion;
         private readonly int mMaxTextLength;
-        private Dictionary<string, Action<string>> mErrorHandlers;
-        private Dictionary<string, string> mUnHandledErrorMessages;
 
         public string Content { get; set; }
         public MarkupLanguage Markup { get; set; }
@@ -31,18 +22,6 @@ namespace Testinator.Server.TestSystem.Implementation
             mVersion = version;
 
             mMaxTextLength = AttributeHelper.GetPropertyAttributeValue<TextContent, string, MaxLenghtAttribute, int>(x => x.Text, a => a.MaxLength, mVersion);
-
-            mErrorHandlers = new Dictionary<string, Action<string>>();
-            mUnHandledErrorMessages = new Dictionary<string, string>();
-
-            // Get all editor methods and create the error handlers map
-            var methodNames = typeof(ITextEditor).GetProperties()
-                              .Where(field => field.GetCustomAttributes<EditorFieldAttribute>().Any())
-                              .Select(field => field.Name)
-                              .ToList();
-
-            mErrorHandlers = methodNames.ToDictionary(k => k, v => default(Action<string>));
-            mUnHandledErrorMessages = methodNames.ToDictionary(k => k, v => default(string));
         }
 
         public OperationResult<ITextContent> Build()
@@ -52,12 +31,12 @@ namespace Testinator.Server.TestSystem.Implementation
             // Verify text
             if (Content.Length > mMaxTextLength)
             {
-                HandleError(x => x.Content, $"Text content is too long. Maximum text length is set to {mMaxTextLength} characters.");
+                HandleErrorFor(x => x.Content, $"Text content is too long. Maximum text length is set to {mMaxTextLength} characters.");
                 veryficationPassed = false;
             }
             if(Markup == MarkupLanguage.Html)
             {
-                HandleError(x => x.Markup, $"Not supported yet.");
+                HandleErrorFor(x => x.Markup, $"Not supported yet.");
                 veryficationPassed = false;
             }
 
@@ -76,34 +55,15 @@ namespace Testinator.Server.TestSystem.Implementation
             { 
                 var result = OperationResult<ITextContent>.Fail();
 
-                var UnhanledErrorMessages = mUnHandledErrorMessages.Values.Where(x => !string.IsNullOrEmpty(x)).ToList();
-                result.AddErrors(UnhanledErrorMessages);
+                // Error messages that were not handled by callback are added to the
+                // operation result list of errors
+                var unhanledErrorMessages = GetUnhandledErrors();
+                result.AddErrors(unhanledErrorMessages);
 
-                // Clear the errors after that
-                mUnHandledErrorMessages.Keys.ToList().ForEach(k => mUnHandledErrorMessages[k] = default);
+                ClearAllErrors();
+
                 return result;
             }
-        }
-
-        private void HandleError(Expression<Func<ITextEditor, object>> propertyExpression, string message)
-        {
-            var propertyName = ExpressionHelpers.GetCorrectPropertyName(propertyExpression);
-            // If there is handler for that method
-            if (mErrorHandlers[propertyName] != null)
-                mErrorHandlers[propertyName].Invoke(message);
-            else
-                mUnHandledErrorMessages[propertyName] = message;
-        }
-
-        public void OnErrorFor(Expression<Func<ITextEditor, object>> propertyExpression, Action<string> action)
-        {
-            var expression = (MemberExpression)propertyExpression.Body;
-            var propertyInfo = (PropertyInfo)expression.Member;
-
-            if (propertyInfo.GetCustomAttributes<EditorFieldAttribute>().Any() == false)
-                throw new ArgumentException($"This property is not an editor property.");
-
-            mErrorHandlers[propertyInfo.Name] = action;
         }
     }
 }
