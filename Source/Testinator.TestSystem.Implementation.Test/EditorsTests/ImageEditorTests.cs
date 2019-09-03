d5359d8be82d9fd1f0f1fae4ea6930aa1a716d06
Maksymilian Lach <maks.lach99@gmail.com>
2019-09-03 20:11:12 +0200

    Image editor tests mock up

---
+++ Source/Testinator.TestSystem.Implementation.Test/EditorsTests/ImageEditorTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using Testinator.Server.TestSystem.Implementation;
+using Testinator.Server.TestSystem.Implementation.Attributes;
+using Testinator.TestSystem.Abstractions.Questions.Task;
+using Xunit;
+
+namespace Testinator.TestSystem.Implementation.Test.EditorsTests
+{
+    public class ImageEditorTests
+    {
+        internal static ImageEditor GetEditor(int version = 1) => new ImageEditorMock(version);
+
+        /*[Fact]
+        public void AddImageCountExceeded()
+        {
+            var editor = GetEditor();
+            editor.AddImage(Image.)
+        }*/
+    }
+
+    internal class ImageEditorMock : ImageEditor
+    {
+        public ImageEditorMock(int version) : base(version)
+        {
+        }
+
+        public ImageEditorMock(IImageContent objToEdit, int version) : base(objToEdit, version)
+        {
+        }
+
+        protected override void LoadAttributeValues()
+        {
+            var mMaxImageCount = AttributeHelper.GetPropertyAttributeValue<ImageContentMock, ICollection<Image>, MaxCollectionCountAttribute, int>
+                (obj => obj.Images, attr => attr.MaxCount, Version);
+
+            var ImageSizeAttr = AttributeHelper.GetPropertyAttribute<ImageContentMock, ICollection<Image>, MaxImageSizeAttribute>
+                (x => x.Images, Version);
+            
+            mMaxImageHeight = ImageSizeAttr.Height;
+            mMaxImageWidth = ImageSizeAttr.Width;
+        }
+    }
+
+    public class ImageContentMock : IImageContent
+    {
+
+        [MaxCollectionCount(maxCount: 5, fromVersion: 1)]
+        [MaxImageSize(width: 1000, height: 1000, fromVersion: 1)]
+        public IList<Image> Images { get; set; }
+    }
+}
