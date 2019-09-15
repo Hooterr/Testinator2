a30771c864a754d548534d9da00be778a7ce59a9
Maksymilian Lach <maks.lach99@gmail.com>
2019-09-15 19:49:45 +0200

    Added TestInfo

--- Source/Testinator.TestSystem.Abstractions/Tests/ITest.cs
+++ Source/Testinator.TestSystem.Abstractions/Tests/ITest.cs
@@ -1,31 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Testinator.TestSystem.Abstractions.Tests
 {
     public interface ITest
     {
         /// <summary>
-        /// The name of this test
+        /// Information about this test
         /// </summary>
-        string Name { get; }
-
-        /// <summary>
-        /// The date when this test was initially created
-        /// </summary>
-        DateTime CreationDate { get; }
-
-        /// <summary>
-        /// The date when this test was last edited
-        /// </summary>
-        DateTime LastEditionDate { get; }
-
-        /// <summary>
-        /// The category of this test
-        /// Can contain subcategories
-        /// </summary>
-        Category Category { get; }
+        ITestInfo Info { get; }
 
         /// <summary>
         /// The criteria attached to this test
@@ -36,11 +18,6 @@ namespace Testinator.TestSystem.Abstractions.Tests
         /// The list of questions attached to this test
         /// </summary>
         IList<IQuestionProvider> Questions { get; }
-
-        /// <summary>
-        /// The time this test can take at most
-        /// </summary>
-        TimeSpan CompletionTime { get; }
 
         /// <summary>
         /// The options for this test to implement
---
+++ Source/Testinator.TestSystem.Abstractions/Tests/ITestInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Testinator.TestSystem.Abstractions.Tests
+{
+    public interface ITestInfo
+    {
+        /// <summary>
+        /// The name of this test
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// The date when this test was initially created
+        /// </summary>
+        DateTime CreationDate { get; }
+
+        /// <summary>
+        /// The date when this test was last edited
+        /// </summary>
+        DateTime LastEditionDate { get; }
+
+        /// <summary>
+        /// The category of this test
+        /// Can contain subcategories
+        /// </summary>
+        Category Category { get; }
+
+        /// <summary>
+        /// The time limit for this test
+        /// </summary>
+        TimeSpan TimeLimit { get; }
+    }
+}
