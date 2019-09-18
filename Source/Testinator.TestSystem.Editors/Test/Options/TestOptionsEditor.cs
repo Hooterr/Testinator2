c641094db4e8cba9d2d0433d787185b4ace1a4b9
Maksymilian Lach <maks.lach99@gmail.com>
2019-09-18 21:38:24 +0200

    TestOptionsEditor comments

--- Source/Testinator.TestSystem.Editors/Test/Options/TestOptionsEditor.cs
+++ Source/Testinator.TestSystem.Editors/Test/Options/TestOptionsEditor.cs
@@ -2,11 +2,29 @@
 
 namespace Testinator.TestSystem.Editors
 {
+    /// <summary>
+    /// Default implementation of <see cref="ITestOptionsEditor"/>
+    /// </summary>
     internal class TestOptionsEditor : BaseEditor<TestOptions, ITestOptionsEditor>, ITestOptionsEditor
     {
+        #region All Constructors
+
+        /// <summary>
+        /// Initializes this editor to edit an existing options pack
+        /// </summary>
+        /// <param name="options">The options to edit</param>
+        /// <param name="version">The version of the test system model to use</param>
         public TestOptionsEditor(TestOptions options, int version) : base(options, version) { }
 
+        /// <summary>
+        /// Initializes this editor to create a new test options pack
+        /// </summary>
+        /// <param name="version">The version of the test system model to use</param>
         public TestOptionsEditor(int version) : base(version) { }
+
+        #endregion
+
+        #region Implementation
 
         protected override TestOptions BuildObject()
         {
@@ -17,6 +35,8 @@ namespace Testinator.TestSystem.Editors
                 result = new TestOptions();
 
             return result;
-        }
+        } 
+
+        #endregion
     }
 }
