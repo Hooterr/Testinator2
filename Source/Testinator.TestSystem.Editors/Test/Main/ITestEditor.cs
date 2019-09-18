511b78f9ed56da6886fb1cdbd2e9f14adfca6f19
Maksymilian Lach <maks.lach99@gmail.com>
2019-09-18 17:15:40 +0200

    TestEditor comments

--- Source/Testinator.TestSystem.Editors/Test/Main/ITestEditor.cs
+++ Source/Testinator.TestSystem.Editors/Test/Main/ITestEditor.cs
@@ -1,22 +1,30 @@
-﻿using Testinator.TestSystem.Abstractions;
-using Testinator.TestSystem.Abstractions.Tests;
+﻿using Testinator.TestSystem.Abstractions.Tests;
 
 namespace Testinator.TestSystem.Editors
 {
+    /// <summary>
+    /// Edits/creates a test
+    /// </summary>
     public interface ITestEditor : IBuildable<ITest> //, TODO nested error listener
     {
-        // TODO: Maybe operation results instead of voids
-        /*void SubmitInfo(ITestInfo testInfo);
-        void SubmitQuestion(IQuestion question);
-        void SubmitOptions(ITestOptions testOptions);
-        void SubmitGrading(IGrading grading);*/
-
+        /// <summary>
+        /// The editor for the info part of the test
+        /// </summary>
         ITestInfoEditor Info { get; }
          
+        /// <summary>
+        /// The editor for the options part of the test
+        /// </summary>
         ITestOptionsEditor Options { get; }
 
+        /// <summary>
+        /// The editor for the grading part of the test
+        /// </summary>
         IGradingEditor Grading { get; }
 
+        /// <summary>
+        /// Question collection of the test
+        /// </summary>
         IQuestionEditorCollection Questions { get; }
     }
 }
