429866d511327393f68b894f824436ae0702a8ca
Minorsonek <minorsonek@gmail.com>
2019-09-19 15:16:28 +0200

    Added InputField helper that adds additional error message functionality to every property that needs it

---
+++ Source/Testinator.Server.Domain/InputString.cs
@@ -0,0 +1,28 @@
+﻿namespace Testinator.Server.Domain
+{
+    public class InputField<T>
+    {
+        public T Value { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public InputField(T value)
+        {
+            Value = value;
+        }
+
+        public static implicit operator T(InputField<T> d)
+        {
+            return d.Value;
+        }
+
+        public static implicit operator InputField<T>(T d)
+        {
+            return new InputField<T>(d);
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
