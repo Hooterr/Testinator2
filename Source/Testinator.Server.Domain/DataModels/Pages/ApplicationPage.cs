e6cf1e706ea5721457f481b3a77dfb3a36e39137
Minorsonek <minorsonek@gmail.com>
2019-09-20 12:16:13 +0200

    Clean up in folders structure

---
+++ Source/Testinator.Server.Domain/DataModels/Pages/ApplicationPage.cs
@@ -0,0 +1,43 @@
+﻿namespace Testinator.Server.Domain
+{
+    /// <summary>
+    /// Every main page in this application as an enum
+    /// </summary>
+    public enum ApplicationPage
+    {
+        /// <summary>
+        /// No page
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// The initial login page
+        /// </summary>
+        Login,
+
+        /// <summary>
+        /// The home page shown after logging in
+        /// </summary>
+        Home,
+
+        /// <summary>
+        /// The test editor initial page
+        /// </summary>
+        TestCreatorInitial,
+
+        /// <summary>
+        /// The screen stream page
+        /// </summary>
+        ScreenStream,
+
+        /// <summary>
+        /// The settings page
+        /// </summary>
+        Settings,
+
+        /// <summary>
+        /// Info about the application
+        /// </summary>
+        About
+    }
+}
---
+++ Source/Testinator.Server.Domain/DataModels/Pages/TestCreatorQuestionsPage.cs
@@ -0,0 +1,9 @@
+﻿namespace Testinator.Server.Domain
+{
+    /// <summary>
+    /// Every page in Test Creator questions section as an enum
+    /// </summary>
+    public enum TestCreatorQuestionsPage
+    {
+    }
+}
