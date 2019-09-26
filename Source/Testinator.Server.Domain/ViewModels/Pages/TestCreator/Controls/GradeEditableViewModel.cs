32c36a9646c41f9037490402c066aacab86a2e0b
Minorsonek <minorsonek@gmail.com>
2019-09-26 15:28:12 +0200

    Added single grade control

---
+++ Source/Testinator.Server.Domain/ViewModels/Pages/TestCreator/Controls/GradeEditableViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Testinator.Core;
+
+namespace Testinator.Server.Domain
+{
+    /// <summary>
+    /// The view model for single editable grade control
+    /// </summary>
+    public class GradeEditableViewModel : BaseViewModel
+    {
+    }
+}
---
+++ Source/Testinator.Server/Controls/TestCreator/GradeEditableControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Testinator.Server
+{
+    /// <summary>
+    /// Interaction logic for GradeEditableControl.xaml
+    /// </summary>
+    public partial class GradeEditableControl : UserControl
+    {
+        public AnswerSelectableControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
