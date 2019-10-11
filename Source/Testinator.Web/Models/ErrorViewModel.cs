aa293cac4f3e80e1adcc000347b0c048be178131
Minorsonek <minorsonek@gmail.com>
2019-10-11 13:59:30 +0200

    Removed old web project

--- Source/Testinator.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Testinator.Web
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
