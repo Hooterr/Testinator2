7e32eb5469d0171db9d94d8aec043cc6bf2f47b8
Minorsonek <minorsonek@gmail.com>
2019-10-16 13:15:20 +0200

    Added simple dashboard page

--- Source/Testinator.WebApp/ViewModels/Pages/DashboardPageViewModel.cs
+++ Source/Testinator.WebApp/ViewModels/Pages/DashboardPageViewModel.cs
@@ -1,9 +1,14 @@
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Windows.Input;
+using Testinator.Core;
+
 namespace Testinator.WebApp
 {
     /// <summary>
     /// The view model for dashboard page
     /// </summary>
-    public class DashboardPageViewModel
+    public class DashboardPageViewModel : BasePageViewModel
     {
         #region Public Properties
 
@@ -16,6 +21,40 @@ namespace Testinator.WebApp
         /// The user's second name (surname)
         /// </summary>
         public string SecondName { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        /// <summary>
+        /// The command to go to test creator initial page
+        /// </summary>
+        public ICommand GoToTestCreatorCommand { get; private set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public DashboardPageViewModel(NavigationManager navigationManager) : base(navigationManager)
+        {
+            // Create commands
+            GoToTestCreatorCommand = new RelayCommand(GoToTestCreator);
+        }
+
+        #endregion
+
+        #region Command Methods
+
+        /// <summary>
+        /// Changes current page to test creator initial page
+        /// </summary>
+        private void GoToTestCreator()
+        {
+            mNavigationManager.NavigateTo("testCreator");
+        }
 
         #endregion
     }
